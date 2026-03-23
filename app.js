6147e148c8b63340757db78e43ae6e6ef3fd4d84
Gonzalo Corvalan <gonzalo.corvalan77@gmail.com>
2026-03-23 20:30:48 -0300

    Conexion a base de datos hecha

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { connectDB } from './src/config/db.js';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server corriendo en puerto ${PORT}`);
+});
---
+++ src/config/db.js
@@ -0,0 +1,26 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pool = mysql.createPool({
+  host:     process.env.DB_HOST,
+  port:     parseInt(process.env.DB_PORT),
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl:      process.env.DB_SSL === 'true' ? { rejectUnauthorized: true } : false,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
+
+const connectDB = async () => {
+  try {
+    await pool.getConnection();
+    console.log('DB conectada');
+  } catch (error) {
+    console.error('Error conectando a la DB:', error.message);
+    process.exit(1);
+  }
+};
+
+export { pool, connectDB };
